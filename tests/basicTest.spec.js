d0de4ac1ac132b778868f7449c3d7d281bf6b74b
Varunreddy1328 <kesireddyvarunreddy08@gmail.com>
2026-06-14 12:34:49 +0530

    basic login test

--- tests/basicTest.spec.js
+++ tests/basicTest.spec.js
@@ -7,12 +7,35 @@ const {test,expect} = require ('@playwright/test');
 //. Destructuring is a JavaScript feature that allows you to extract values from arrays or properties from objects and assign them to variables in a more concise way.
 //  In this case, we are using destructuring to extract the test function from the @playwright/test module, allowing us to use it directly in our code without having to reference the module each time.
 
-test('first test',async ({browser})=> {
+test.only('first test',async ({browser})=> {
  
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    // css and xpath selectors are used to locate elements on a web page.
+    //  CSS selectors are based on the HTML structure and use CSS syntax, while XPath selectors use a path-like syntax to navigate the HTML document.
 
+    const userName = page.locator('#username');
+    const password = page.locator('#password');
+    const signInButton = page.locator('#signInBtn');
+    const cardTitles = page.locator('.card-body a');
+    const errorMessage = page.locator("[style*='block']");
+
+    await userName.fill('rahulshettyacademy');
+    await password.fill('Learning@830$3mK2');
+
+    await signInButton.click();
+
+    if(await errorMessage.isVisible()){
+    console.log(await errorMessage.textContent());
+    await expect(errorMessage).toContainText("Incorrect");
+    }
+    else{
+        console.log(await cardTitles.nth(0).textContent());
+    }
+
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
 })
 
 test('page fixture example',async ({page})=> {
